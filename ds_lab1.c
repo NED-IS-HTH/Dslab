da2e1f65938affc0a9eafd59de986a81da6e76b1
NED-IS-HTH <newnedis17@gmail.com>
2025-01-16 01:51:25 +0530

    Add files via upload

---
+++ ds_lab1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void Crt_arr(int n, int a[])
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter element %d: ", i + 1);
+        scanf("%d", &a[i]);
+    }
+}
+
+void Trav_arr(int a[])
+{
+    for(int i=0; i < )
+}
+
+int main()
+{
+    int n, arr[100];
+
+    printf("Enter the no. of elements: ");
+    scanf("%d",&n);
+    Crt_arr(n, arr);
+    
+
+    return 0;
+}
